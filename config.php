368dd62dc7a9a4a2b2d0e0a75446dc3c8c9b4051
memish <mmemmo@gmail.com>
2023-07-08 18:26:49 -0400

    Create config.php
    This file should be in a secure directory

---
+++ config.php
@@ -0,0 +1,13 @@
+<?php
+
+
+// Database connection parameters
+$dbHost = "host_address";
+$dbUsername = "DBusername";
+$dbPassword = "DBpassword";
+$dbName = "DBname";
+
+// Create a MySQLi connection
+$connection = new mysqli($dbHost, $dbUsername, $dbPassword, $dbName);
+
+?>
